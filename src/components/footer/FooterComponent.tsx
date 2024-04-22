d0982878eb9ca9da232c844730237001b42deb35
Roko <rokoponjarac2@gmail.com>
2024-04-22 15:06:22 +0200

    add logo

--- src/components/footer/FooterComponent.tsx
+++ src/components/footer/FooterComponent.tsx
@@ -1,14 +1,37 @@
 import styles from "./FooterComponent.module.scss";
+import { Link } from "react-router-dom";
+import { userRoleManager } from "../../util/userRoleContext";
+
 const FooterComponent = () => {
+  const { role } = userRoleManager();
+
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footer__wave}>
-        <img src="/blue-background-wave.svg" alt="blue-background-wave" />
-      </div>
-      <div className={styles.footer__box}>
-        <div className={styles.footer__box__container}></div>
-      </div>
-    </footer>
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.footer__wave}>
+          <img src="/blue-background-wave.svg" alt="blue-background-wave" />
+        </div>
+        <div className={styles.footer__box}>
+          <div className={styles.footer__box__container}>
+            <Link
+              to={role == "admin" ? "/admin" : "/"}
+              className={styles.footer__box__logo}
+            >
+              <img
+                src={"/logo-mobile.svg"}
+                alt="mobile-logo"
+                className={styles.footer__box__logo__mobile}
+              />
+              <img
+                src={"/logo-desktop.svg"}
+                alt="mobile-logo"
+                className={styles.footer__box__logo__desktop}
+              />
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 

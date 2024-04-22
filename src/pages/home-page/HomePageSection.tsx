ccd9c1691264052f7161e2da09718e95a4db4254
Roko <rokoponjarac2@gmail.com>
2024-04-22 19:06:03 +0200

    make container

--- src/pages/home-page/HomePageSection.tsx
+++ src/pages/home-page/HomePageSection.tsx
@@ -8,9 +8,11 @@ const HomePageSection = () => {
         variant={"main"}
         title={"Dobrodošli na Edit Code School"}
       />
-      <section className={styles.home_section}>
-        <span className={styles.home_section__small_title}>RADIONICE</span>
-      </section>
+      <div className={styles.container}>
+        <section className={styles.home_section}>
+          <span className={styles.home_section__small_title}>RADIONICE</span>
+        </section>
+      </div>
     </>
   );
 };

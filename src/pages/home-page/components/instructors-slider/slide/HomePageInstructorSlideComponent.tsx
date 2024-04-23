7d1f314e5edeb6bf3e8e253e9bde820f64171ead
Roko <rokoponjarac2@gmail.com>
2024-04-23 18:21:12 +0200

    make style for slider and slide

--- src/pages/home-page/components/instructors-slider/slide/HomePageInstructorSlideComponent.tsx
+++ src/pages/home-page/components/instructors-slider/slide/HomePageInstructorSlideComponent.tsx
@@ -1,4 +1,6 @@
-import "./HomePageInstructorSlideComponent.scss";
+import "./instructor-slide.scss";
+
+import styles from "./HomePageInstructorSlideComponent.module.scss";
 
 interface HomePageInstructorSlideComponentProps {
   instructor: Instructor;
@@ -7,7 +9,19 @@ interface HomePageInstructorSlideComponentProps {
 const HomePageInstructorSlideComponent: React.FC<
   HomePageInstructorSlideComponentProps
 > = ({ instructor }) => {
-  return <div>jA</div>;
+  return (
+    <div className={styles.instructor_slide}>
+      <div className={styles.instructor_slide__image}>
+        <img src={"/background-banner.png"} alt="instructor-image" />
+      </div>
+      <div className={styles.instructor_slide__info}>
+        <h2 className={styles.instructor_slide__info__name}>
+          {instructor.name}
+        </h2>
+        <span>{instructor.organization}</span>
+      </div>
+    </div>
+  );
 };
 
 export default HomePageInstructorSlideComponent;

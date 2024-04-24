3efb2a324801f800d84eacd99c1bdcded5d4ae57
Roko <rokoponjarac2@gmail.com>
2024-04-24 17:18:40 +0200

    refactor tabs link

--- src/pages/admin-page/AdminPageSection.tsx
+++ src/pages/admin-page/AdminPageSection.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import BannerComponent from "../../components/banner/BannerComponent";
 import styles from "./AdminPageSection.module.scss";
+import AdminPageTabsLinkComponent from "./components/tabs-links/AdminPageTabsLinkComponent";
 
 const AdminPageSection = () => {
   const [tabActiveLink, setTabActiveLink] = useState<
@@ -14,50 +15,12 @@ const AdminPageSection = () => {
       <BannerComponent variant={"secondary"} title={"Administracija"} />
       <div className={styles.container}>
         <section className={styles.admin_section}>
-          <ul className={styles.admin_section__tabs_links}>
-            <li
-              onClick={() => setTabActiveLink("workshops")}
-              className={`${
-                tabActiveLink == "workshops" ? styles.tab_active : ""
-              }`}
-            >
-              Radionice
-            </li>
-            <li
-              onClick={() => setTabActiveLink("instructors")}
-              className={`${
-                tabActiveLink == "instructors" ? styles.tab_active : ""
-              }`}
-            >
-              Predavači
-            </li>
-            <li
-              onClick={() => setTabActiveLink("organizations")}
-              className={`${
-                tabActiveLink == "organizations" ? styles.tab_active : ""
-              }`}
-            >
-              Organizacije
-            </li>
-          </ul>
-          <select
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-              setTabActiveLink(
-                `${
-                  e.target.value as
-                    | "workshops"
-                    | "instructors"
-                    | "organizations"
-                }`
-              )
-            }
-            value={tabActiveLink}
-            className={styles.admin_section__tabs_select}
-          >
-            <option value={"workshops"}>Radionice</option>
-            <option value={"instructors"}>Predavači</option>
-            <option value={"organizations"}>Organizacije</option>
-          </select>
+          <AdminPageTabsLinkComponent
+            tabActiveLink={tabActiveLink}
+            setTabActiveLink={(
+              value: "workshops" | "instructors" | "organizations"
+            ) => setTabActiveLink(value)}
+          />
         </section>
       </div>
     </>

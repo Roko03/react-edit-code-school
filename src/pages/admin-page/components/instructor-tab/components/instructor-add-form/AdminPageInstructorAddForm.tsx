3ae55fe970a63219542e106388809ef985db6fd7
Roko <rokoponjarac2@gmail.com>
2024-04-30 20:09:57 +0200

    add organization list and file input

--- src/pages/admin-page/components/instructor-tab/components/instructor-add-form/AdminPageInstructorAddForm.tsx
+++ src/pages/admin-page/components/instructor-tab/components/instructor-add-form/AdminPageInstructorAddForm.tsx
@@ -3,6 +3,9 @@ import styles from "./AdminPageInstructorAddForm.module.scss";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import ButtonComponent from "../../../../../../components/button/ButtonComponent";
+import { useEffect, useState } from "react";
+import CircularProgressComponent from "../../../../../../components/circular-progress/CircularProgressComponent";
+import getOrganizations from "../../../../../../lib/getOrganizations";
 
 const addInstructorSchema = z.object({
   name: z.string().min(1, { message: "Unesite ime" }),
@@ -10,7 +13,7 @@ const addInstructorSchema = z.object({
     .string()
     .min(5, { message: "Morate unijeti informacije o predavaču" })
     .max(350, { message: "Ne smije imati poviše 350 znakova" }),
-  organization: z.array(z.object({ organizationId: z.string() })),
+  organization: z.union([z.string(), z.array(z.string())]),
 });
 
 type TAddInstructorSchema = z.infer<typeof addInstructorSchema>;
@@ -25,10 +28,26 @@ const AdminPageInstructorAddForm = () => {
     resolver: zodResolver(addInstructorSchema),
   });
 
+  const [organizationList, setOrganizationList] = useState<
+    Organization[] | null
+  >(null);
+
+  const fetchOrganizations = async () => {
+    const response = await getOrganizations();
+
+    setOrganizationList(response);
+  };
+
   const onSubmit = (data: TAddInstructorSchema) => {
     reset();
     console.log(data);
   };
+
+  useEffect(() => {
+    fetchOrganizations();
+  }, []);
+
+  if (organizationList == null) return <CircularProgressComponent />;
 
   return (
     <form className={styles.add_form} onSubmit={handleSubmit(onSubmit)}>
@@ -39,6 +58,9 @@ const AdminPageInstructorAddForm = () => {
         )}
       </label>
       <label>
+        <input type="file" accept="image/*" />
+      </label>
+      <label>
         <textarea {...register("biography")} placeholder="Unesite podatke" />
         {errors.biography && (
           <p
@@ -47,14 +69,26 @@ const AdminPageInstructorAddForm = () => {
         )}
       </label>
       <div className={styles.add_form__checkboxs}>
-        <div className={styles.add_form__checkboxs__item}>
-          <input type="checkbox" id="id1" />
-          <label htmlFor={"id1"}>Label 1</label>
-        </div>
-        <div className={styles.add_form__checkboxs__item}>
-          <input type="checkbox" id="id2" />
-          <label htmlFor={"id2"}>Label 2</label>
-        </div>
+        {organizationList.map((organizationItem, index) => {
+          return (
+            <div className={styles.add_form__checkboxs__item} key={index}>
+              <input
+                type="checkbox"
+                id={`${organizationItem.id}`}
+                value={organizationItem.id}
+                {...register("organization")}
+              />
+              <label htmlFor={`${organizationItem.id}`}>
+                {organizationItem.name}
+              </label>
+            </div>
+          );
+        })}
+        {errors.organization && (
+          <p
+            className={styles.add_form__error}
+          >{`${errors.organization.message}`}</p>
+        )}
       </div>
       <ButtonComponent variant={"add"} onClick={handleSubmit(onSubmit)}>
         <p>Napravi predavača</p>

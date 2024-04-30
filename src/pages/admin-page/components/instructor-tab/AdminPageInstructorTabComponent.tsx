01367a22fbfef0adf69a155042b4f06b3389baf8
Roko <rokoponjarac2@gmail.com>
2024-04-30 19:08:29 +0200

    set main instructor tab

--- src/pages/admin-page/components/instructor-tab/AdminPageInstructorTabComponent.tsx
+++ src/pages/admin-page/components/instructor-tab/AdminPageInstructorTabComponent.tsx
@@ -3,12 +3,22 @@ import getInstructors from "../../../../lib/getInstructors";
 import CircularProgressComponent from "../../../../components/circular-progress/CircularProgressComponent";
 import ButtonComponent from "../../../../components/button/ButtonComponent";
 import AdminPageInstructorListComponent from "./instructor-list/AdminPageInstructorListComponent";
+import AdminPageModalComponent from "../modal/AdminPageModalComponent";
+import DialogComponent from "../../../../components/dialog/DialogComponent";
+import SnackBarComponent from "../../../../components/snack-bar/SnackBarComponent";
 
 const AdminPageInstructorTabComponent = () => {
   const [instructorList, setInstructorList] = useState<Instructor[] | null>(
     null
   );
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalType, setModalType] = useState<"add" | "delete" | "edit" | null>(
+    null
+  );
+
+  const [isSuccessful, setIsSuccessful] = useState<boolean | null>(null);
+  const [snackBarMessage, setSnackBarMessage] = useState<string>("");
 
   const fetchInstructors = async () => {
     setIsLoading(true);
@@ -28,12 +38,51 @@ const AdminPageInstructorTabComponent = () => {
 
   return (
     <>
-      <ButtonComponent variant={"add"}>
+      <ButtonComponent
+        variant={"add"}
+        onClick={() => {
+          setIsModalOpen(true);
+          setModalType("add");
+        }}
+      >
         <img src={"/plus.svg"} alt="plus" />
         <p>Dodaj predavača</p>
       </ButtonComponent>
       {instructorList != null && (
         <AdminPageInstructorListComponent instructorList={instructorList} />
+      )}
+      <DialogComponent
+        isOpen={isModalOpen}
+        closeDialog={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        <AdminPageModalComponent actionType={modalType}>
+          {modalType == "add" ? (
+            <p>Add</p>
+          ) : modalType == "delete" ? (
+            <p>Delete</p>
+          ) : (
+            <p>Edit</p>
+          )}
+        </AdminPageModalComponent>
+      </DialogComponent>
+      {isSuccessful == null ? (
+        <></>
+      ) : isSuccessful ? (
+        <SnackBarComponent
+          variant={"successful"}
+          onClick={() => setIsSuccessful(null)}
+        >
+          <p>{snackBarMessage}</p>
+        </SnackBarComponent>
+      ) : (
+        <SnackBarComponent
+          variant={"error"}
+          onClick={() => setIsSuccessful(null)}
+        >
+          <p>{snackBarMessage}</p>
+        </SnackBarComponent>
       )}
     </>
   );

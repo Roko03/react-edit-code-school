1ee7294a637d51bb55917cda034bbb5ccc74c68c
Roko <rokoponjarac2@gmail.com>
2024-05-01 13:09:56 +0200

    make delet instructor function

---
+++ src/lib/deleteInstructor.ts
@@ -0,0 +1,9 @@
+export default async function deleteInstructor(id: string) {
+    const response = await fetch(`${import.meta.env.VITE_PUBLIC_BACK_BASE_URL}/predavac/${id}`, {
+        method: 'DELETE'
+    })
+
+    if (!response.ok) return { success: false, message: `Ne možete izbrisati traženog predavača` }
+
+    return { success: true, message: `Predavač je izbrisana` }
+}

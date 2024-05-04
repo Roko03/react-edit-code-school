766912067a431bbdacd7e8af341f865056ec139d
Roko <rokoponjarac2@gmail.com>
2024-05-04 20:10:34 +0200

    add filters

--- src/pages/instructor-page/InstructorPageSection.tsx
+++ src/pages/instructor-page/InstructorPageSection.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import BannerComponent from "../../components/banner/BannerComponent";
+import FilterComponent from "../../components/filter/FilterComponent";
 import styles from "./InstructorPageSection.module.scss";
+import { useSearchParams } from "react-router-dom";
 
 const InstructorPageSection = () => {
+  const [searchParams] = useSearchParams();
+
+  const getFiltersByParams = () => {
+    const filters = searchParams.get("filter");
+
+    if (filters) {
+      return filters.split(",");
+    }
+
+    return [];
+  };
+  const [filters, setFilters] = useState<string[]>(getFiltersByParams());
+
   return (
     <>
       <BannerComponent variant={"secondary"} title={"Predavači"} />
       <div className={styles.container}>
-        <section className={styles.instructor_section}></section>
+        <section className={styles.instructor_section}>
+          <FilterComponent
+            variant={"instructor"}
+            filters={filters}
+            setFiltersArray={(array: string[]) => setFilters(array)}
+            clearFilters={() => setFilters([])}
+          />
+        </section>
       </div>
     </>
   );
